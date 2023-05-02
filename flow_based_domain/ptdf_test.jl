5d6395b6f1a2d6b39d29ecf0c754abc25625c333
Balazs Riskutia <balazs.riskutia@gmail.com>
2023-05-02 10:53:52 +0200

    ptdf optimisation

--- flow_based_domain/ptdf_test.jl
+++ flow_based_domain/ptdf_test.jl
@@ -17,10 +17,10 @@ O = npzread("./flow_based_domain/omega_matrix.npy")
 
 M = zeros(size(O)[2], size(O)[1])
 X = zeros(size(O)[3], size(GSK1_P)[2])
-#X[1, :] .= 1
-X[1, :] .= 0.6
-X[2, :] .= 0.2
-X[3, :] .= 0.2
+X[1, :] .= 1
+#X[1, :] .= 0.6
+#X[2, :] .= 0.2
+#X[3, :] .= 0.2
 #X[:, :] .= 0.2
 
 for p in 1:size(GSK1_P)[2]
@@ -70,7 +70,7 @@ plot(df_ptdf[df_ptdf.line_id .== 815, :DateTime], df_ptdf[df_ptdf.line_id .== 81
 plot_ptdf(800, 9, -0.026198)
 """
 
-function generate_PTDF(line, contingency, zone, centring_value)
+function generate_PTDF(line, edge_number, contingency, zone, centring_value)
     ptdf_z_obs_x = df_ptdf[(df_ptdf.line_id .== line) .& (df_ptdf.contingency .== contingency), :DateTime]
     ptdf_z_obs_y = df_ptdf[(df_ptdf.line_id .== line) .& (df_ptdf.contingency .== contingency), zone]
     plot(ptdf_z_obs_x, ptdf_z_obs_y)
@@ -104,24 +104,24 @@ function generate_PTDF(line, contingency, zone, centring_value)
     zone_i = findall(zones.==zone)[1]
     ptdf1_line_t = Array{Float64}(undef, size(GSK1_P)[1])
     for t = 1:size(GSK1_P)[1]
-        ptdf1_line_t[t] = PTDF1_Z_T[t][line, zone_i] + centring_value
+        ptdf1_line_t[t] = PTDF1_Z_T[t][edge_number + 1, zone_i]# + centring_value
     end
 
     ptdf2_line_t = Array{Float64}(undef, size(GSK2_P)[1])
     for t = 1:size(GSK2_P)[1]
-        ptdf2_line_t[t] = PTDF2_Z_T[t][line, zone_i] + centring_value
+        ptdf2_line_t[t] = PTDF2_Z_T[t][edge_number + 1, zone_i]# + centring_value
     end
 
     ptdf3_line_t = Array{Float64}(undef, size(GSK3_P)[1])
     for t = 1:size(GSK3_P)[1]
-        ptdf3_line_t[t] = PTDF3_Z_T[t][line, zone_i] + centring_value
+        ptdf3_line_t[t] = PTDF3_Z_T[t][edge_number + 1, zone_i]# + centring_value
     end
 
     ptdf5_line_t = Array{Float64}(undef, size(GSK5_P)[1])
     for t = 1:size(GSK5_P)[1]
-        ptdf5_line_t[t] = PTDF5_Z_T[t][line, zone_i] + centring_value
+        ptdf5_line_t[t] = PTDF5_Z_T[t][edge_number + 1, zone_i]# + centring_value
     end
 
-    #plot!(twiny(), [ptdf1_line_t, ptdf2_line_t, ptdf3_line_t, ptdf5_line_t])
-    plot([ptdf1_line_t, ptdf2_line_t, ptdf3_line_t, ptdf5_line_t])
+    plot!(twiny(), [ptdf1_line_t, ptdf2_line_t, ptdf3_line_t, ptdf5_line_t])
+    #plot([ptdf1_line_t, ptdf2_line_t, ptdf3_line_t, ptdf5_line_t])
 end
